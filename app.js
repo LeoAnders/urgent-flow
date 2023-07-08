fac5f45a0cad2799c5811100eb2e0bb4b9862e5e
LeoAnders <leonardoanders15@gmail.com>
2023-07-08 19:12:55 -0300

    Estizando pagina de urgentes

--- app.js
+++ app.js
@@ -21,5 +21,7 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'templates'))  
 
 app.use("/", requestRoute);
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.listen(port, ()=> console.log(`Server running on port ${port}`));
