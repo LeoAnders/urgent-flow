8925d4b18605097d2cff5d8928996e7d4415237f
LeoAnders <leonardoanders15@gmail.com>
2023-07-11 23:23:39 -0300

    Configurando arquivo .env para hospedar projeto

--- app.js
+++ app.js
@@ -2,23 +2,14 @@ const express = require("express");
 const app = express();
 const path = require('path');
 const port = 3000;
-const mongoose = require("mongoose");
 const requestRoute = require("./routes/requestRoute");
 
-mongoose.connect("mongodb://localhost/newrequests", {
-  useNewUrlParser: true,  
-  useUnifiedTopology: true});
-
-let db = mongoose.connection
-
-db.on("error", ()=> {
-  console.log("Error connecting to database");});
-
-db.once("open", ()=> {
-  console.log("Connected to database");});
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'templates'))  
+
+const mongo = require("./database/mongo")
+mongo()
 
 app.use("/", requestRoute);
 app.use(express.static(path.join(__dirname, 'public')));
---
+++ database/mongo.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const mongoose = require("mongoose");
+
+async function main() {
+  try {
+    mongoose.set("strictQuery", true);
+
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to database");
+  } catch (error) {
+    console.log("Error connecting to database", error);
+  }
+}
+
+module.exports = main;
