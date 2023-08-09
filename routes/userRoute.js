e65a3f3e83b0440b29a221ff6d37f067c6a58ab4
LeoAnders <leonardoanders15@gmail.com>
2023-08-08 22:28:51 -0300

    Adicionando botao para Add usuario, com rota restrita

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const router = express.Router()
 const userController = require("../controllers/userController")
+const { ensureAuthenticated } = require("../controllers/authController")
 
 router.get("/login", userController.userLogin)
-router.get("/register", userController.userRegister)
+router.get("/register",ensureAuthenticated, userController.userRegister)
 router.get("/logout", userController.userLogout)
 
 router.post("/register", express.urlencoded({ extended:true }), userController.registerHandle)
